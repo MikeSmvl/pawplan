1f6ccc786221f721a6c2374357bf924f9a44f114
Mike Samvelian <mikael.samvelian@gmail.com>
2024-11-07 01:08:51 -0500

    refactor: Rename "tasks" to "behaviors" in the Home page

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -40,7 +40,7 @@ const HeroPage = () => {
                 <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-64 flex-grow flex flex-col justify-center">
                     <div className="text-center">
                         <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
-                            Transform Your Dog&apos;s Behavior
+                            Plan Your Dog’s Training Journey
                         </h1>
                         <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
                             AI-powered insights to help you understand, prioritize, and improve your dog&apos;s behavior.
